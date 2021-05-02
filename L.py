e93bed1ebab26248d5646e717b2bc9f8dea94762
NicolasHidalgoBucarey <71038725+NicolasHidalgoBucarey@users.noreply.github.com>
2021-05-02 17:47:26 -0400

    agr paciente-medico
    agregando paciente y medico campos

--- L.py
+++ L.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 #Definir la ventana
 root=Tk()
-root.geometry("600x400")
+root.geometry("1200x600")
 root.title("Centro de Salud")
 root.resizable(0,0)
 
@@ -10,35 +10,56 @@ def salud():
     salud_label.config(
         bg="white",
         font=("Arial",18),
-        padx=220
+        padx=520
     )
     salud_label.grid(row=0, column=0, columnspan=6)
 
-    #Opciones
+    #Opciones paciente
     salud_frame.grid(row=1)
-    salud_paciente_label.grid(row=1,column=0, padx=5, pady=5, sticky=E)
-    salud_paciente_entry.grid(row=1,column=1, padx=5, pady=5, sticky=W)
+    salud_paciente_label.grid(row=1,column=1, padx=5, pady=5, sticky=W)
 
-    salud_medico_label.grid(row=2,column=0, padx=5, pady=5,sticky=E)
-    salud_medico_entry.grid(row=2,column=1, padx=5, pady=5,sticky=W)
+    salud_nombre_label.grid(row=2,column=0, padx=5, pady=5, sticky=E)
+    salud_paciente_n_entry.grid(row=2,column=1, padx=5, pady=5,sticky=W)
 
-    botonA.grid(row=1,column=3, sticky=W)
+    salud_apellido_label.grid(row=3,column=0, padx=5, pady=5, sticky=E)
+    salud_paciente_a_entry.grid(row=3,column=1, padx=5, pady=5,sticky=W)
+
+    salud_fono_label.grid(row=4,column=0, padx=5, pady=5, sticky=E)
+    salud_paciente_f_entry.grid(row=4,column=1, padx=5, pady=5,sticky=W)
+
+    #Opciones medico
+
+    salud_medico_label.grid(row=5,column=1, padx=5, pady=5, sticky=W)
+
+    salud_nombreM_label.grid(row=6,column=0, padx=5, pady=5, sticky=E)
+    salud_medico_n_entry.grid(row=6,column=1, padx=5, pady=5,sticky=W)
+
+    salud_apellidoM_label.grid(row=7,column=0, padx=5, pady=5, sticky=E)
+    salud_medico_a_entry.grid(row=7,column=1, padx=5, pady=5,sticky=W)
+
+    salud_fonoM_label.grid(row=8,column=0, padx=5, pady=5, sticky=E)
+    salud_medico_f_entry.grid(row=8,column=1, padx=5, pady=5,sticky=W)
+
+    salud_especialidad_label.grid(row=9,column=0, padx=5, pady=5, sticky=E)
+    salud_medico_e_entry.grid(row=9,column=1, padx=5, pady=5,sticky=W)
+
+    botonA.grid(row=10,column=1, sticky=W)
     botonA.config(cursor="hand2", bg="green")
 
-    botonE.grid(row=1,column=4, sticky=W)
+    botonE.grid(row=10,column=2, sticky=W)
     botonE.config(cursor="hand2", bg="red")
 
-    botonB.grid(row=1,column=5, sticky=W)
+    botonB.grid(row=10,column=3, sticky=W)
     botonB.config(cursor="hand2")
 
 
-    botonAM.grid(row=2,column=3, sticky=W)
+    botonAM.grid(row=10,column=1, sticky=W)
     botonAM.config(cursor="hand2", bg="green")
 
-    botonEM.grid(row=2,column=4, sticky=W)
+    botonEM.grid(row=10,column=2, sticky=W)
     botonEM.config(cursor="hand2",bg="red")
 
-    botonBM.grid(row=2,column=5, sticky=W)
+    botonBM.grid(row=10,column=3, sticky=W)
     botonBM.config(cursor="hand2")
 
 
@@ -62,16 +83,40 @@ def alertas():
 #Variables
 nomP=StringVar()
 nomM=StringVar()
+
+nombreP=StringVar()
+apellidoP=StringVar()
+fonoP=IntVar()
+
+nombreM=StringVar()
+apellidoM=StringVar()
+fonoM=IntVar()
+especialidad=StringVar()
+
 #Definir campos salud
 salud_label= Label(root, text="Centro de salud")
 
 #Opciones centro de salud
 salud_frame=Frame(root)
-salud_paciente_label=Label(salud_frame, text="Nombre del paciente: ")
-salud_paciente_entry=Entry(salud_frame,textvariable=nomP)
+salud_paciente_label=Label(salud_frame, text="Paciente")
+salud_nombre_label=Label(salud_frame,   text="Nombre :")
+salud_apellido_label=Label(salud_frame, text="Apellido :")
+salud_fono_label=Label(salud_frame,     text="Fono :")
 
-salud_medico_label=Label(salud_frame, text="Nombre del Profesional: ")
-salud_medico_entry=Entry(salud_frame,textvariable=nomM)
+salud_paciente_n_entry=Entry(salud_frame,textvariable=nombreP)
+salud_paciente_a_entry=Entry(salud_frame,textvariable=apellidoP)
+salud_paciente_f_entry=Entry(salud_frame,textvariable=fonoP)
+
+salud_medico_label=Label(salud_frame,        text="Profesional")
+salud_nombreM_label=Label(salud_frame,       text="Nombre :")
+salud_apellidoM_label=Label(salud_frame,     text="Apellido :")
+salud_fonoM_label=Label(salud_frame,         text="Fono :")
+salud_especialidad_label=Label(salud_frame,  text="Especialidad :")
+
+salud_medico_n_entry=Entry(salud_frame,textvariable=nombreM)
+salud_medico_a_entry=Entry(salud_frame,textvariable=apellidoM)
+salud_medico_f_entry=Entry(salud_frame,textvariable=fonoM)
+salud_medico_e_entry=Entry(salud_frame,textvariable=especialidad)
 
 botonA=Button(salud_frame,text="Agregar")
 botonE=Button(salud_frame,text="Eliminar")
